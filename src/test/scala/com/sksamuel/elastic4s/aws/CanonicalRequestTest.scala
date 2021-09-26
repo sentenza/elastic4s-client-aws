d89073eeff74df9a53cbdb9192f18356239f4605
PJ Fanning <pjfanning@users.noreply.github.com>
2021-09-26 18:24:30 +0100

    Update CanonicalRequestTest.scala

--- src/test/scala/com/sksamuel/elastic4s/aws/CanonicalRequestTest.scala
+++ src/test/scala/com/sksamuel/elastic4s/aws/CanonicalRequestTest.scala
@@ -3,13 +3,15 @@ package com.sksamuel.elastic4s.aws
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 
+import java.net.URLEncoder
+
 class CanonicalRequestTest extends AnyWordSpec with Matchers with SharedTestData {
 
   "CanonicalRequest" should {
 
     val resultWithoutPayload =
-      """GET
-        |/path/to/resource
+      s"""GET
+        |/${URLEncoder.encode("https://es.amazonaws.com", "iso-8859-1")}/path/to/resource
         |Action=ListUsers&Version=2010-05-08
         |content-type:application/x-www-form-urlencoded; charset=utf-8
         |host:es.amazonaws.com
@@ -20,7 +22,7 @@ class CanonicalRequestTest extends AnyWordSpec with Matchers with SharedTestData
 
     val resultWithForbiddenCharacters =
       s"""GET
-         |/path/to/resource$encodedForbiddenCharactersAndMore
+         |/${URLEncoder.encode("https://es.amazonaws.com", "iso-8859-1")}/path/to/resource$encodedForbiddenCharactersAndMore
          |Action=ListUsers&Version=2010-05-08
          |content-type:application/x-www-form-urlencoded; charset=utf-8
          |host:es.amazonaws.com
@@ -30,8 +32,8 @@ class CanonicalRequestTest extends AnyWordSpec with Matchers with SharedTestData
          |e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855""".stripMargin
 
     val resultWithPayload =
-      """POST
-        |/path/to/resource
+      s"""POST
+        |/${URLEncoder.encode("https://es.amazonaws.com", "iso-8859-1")}/path/to/resource
         |Action=ListUsers&Version=2010-05-08
         |content-type:application/x-www-form-urlencoded; charset=utf-8
         |host:es.amazonaws.com
@@ -40,22 +42,22 @@ class CanonicalRequestTest extends AnyWordSpec with Matchers with SharedTestData
         |content-type;host;x-amz-date
         |9a5de716a2c94f29b95a8cbfab004d1c8c8c5c1fd8781f1fe7916796b7633b95""".stripMargin
 
-    "be able to build a canonical request string from request without payload" ignore {
+    "be able to build a canonical request string from request without payload" in {
       val canonicalRequest = CanonicalRequest(httpGetRequest)
       canonicalRequest.toString shouldBe(resultWithoutPayload)
     }
 
-    "be able to build a canonical request string from request with unordered query params" ignore {
+    "be able to build a canonical request string from request with unordered query params" in {
       val canonicalRequest = CanonicalRequest(httpGetRequestWithUnorderedQueryParams)
       canonicalRequest.toString shouldBe(resultWithoutPayload)
     }
 
-    "be able to build a canonical request string from request with payload" ignore {
+    "be able to build a canonical request string from request with payload" in {
       val canonicalRequest = CanonicalRequest(httpPostRequest)
       canonicalRequest.toString shouldBe(resultWithPayload)
     }
 
-    "be able to be encode a url with forbidden characters making sure it follows RFC 3986" ignore {
+    "be able to be encode a url with forbidden characters making sure it follows RFC 3986" in {
       val canonicalRequest = CanonicalRequest(httpWithForbiddenCharacters)
       canonicalRequest.toString shouldBe(resultWithForbiddenCharacters)
     }
